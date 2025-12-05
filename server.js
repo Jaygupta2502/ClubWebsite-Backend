dcb06bdd922651335d84f78cf31db24e5211238f
Jay Gupta <153534164+Jaygupta2502@users.noreply.github.com>
2025-12-05 22:12:04 +0530

    Edited server.js4

--- server.js
+++ server.js
@@ -13,24 +13,16 @@ const allowedOrigins = [
   'https://clubwebsite-frontend.onrender.com'
 ];
 
-//app.use(cors({
- // origin: function(origin, callback) {
-    // allow server-to-server no-origin requests
-    //if (!origin) return callback(null, true);
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
 
-    //if (allowedOrigins.indexOf(origin) === -1) {
-    //  return callback(new Error('Not allowed by CORS'), false);
-   // }
-   // return callback(null, true);
-  //},
-  //credentials: true,
-//}));
-
-// Handle preflight
-app.options('*', cors({
+app.options('/api/*', cors({
   origin: allowedOrigins,
   credentials: true
 }));
+
 
 // ===== BODY PARSERS =====
 app.use(express.json());
